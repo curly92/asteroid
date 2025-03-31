be8d44120d7c6bd83654ed70343b661f361e47a7
curly92 <curlyhasenbein@yahoo.com>
2025-03-31 16:30:18 -0700

    have player movement working now and I am using groups to call methods rather than calling individually

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -14,7 +19,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen=screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         ms = game_clock.tick(60)
         dt = ms/1000
