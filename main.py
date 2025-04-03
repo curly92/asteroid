960fe13479bfc22a45fff7f0293fc8528a6d47d7
curly92 <curlyhasenbein@yahoo.com>
2025-04-02 18:47:49 -0700

    finished the bare minimum for the asteroid game

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if shot.isColliding(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         for sprite in drawable:
             sprite.draw(screen)
